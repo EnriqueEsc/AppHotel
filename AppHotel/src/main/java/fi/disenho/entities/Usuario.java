b2ce7141d869151163055b24885377963bdad491
EnriqueEsc <119605520+EnriqueEsc@users.noreply.github.com>
2023-12-06 14:30:17 -0600

    V6-12-23

--- AppHotel/src/main/java/fi/disenho/entities/Usuario.java
+++ AppHotel/src/main/java/fi/disenho/entities/Usuario.java
@@ -6,16 +6,16 @@ package fi.disenho.entities;
 
 import java.io.Serializable;
 import java.util.Date;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 /**
  *
---
+++ AppHotel/src/main/java/fi/disenio/backing/LoginBean.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package fi.disenio.backing;
+
+
+import jakarta.ejb.EJB;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import fi.disenho.entities.*;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.Locale;
+
+/**
+ *
+ * @author jcs
+ */
+@Named(value = "loginBean")
+@SessionScoped
+public class LoginBean implements Serializable {
+    
+    @Size(message = "El campo corroe no es válido", min = 1)
+    private String correo;
+    
+    @Size(min = 1, max = 8, message = "Longitud de password no válido")
+    private String passwd;
+    
+    private Usuario usuario;
+    
+    
+    private Locale locale = new Locale("es", "MX");
+    
+    
+    
+    public void cambiarIdioma(String lg, String pais){
+        locale = new Locale(lg, pais);
+    }
+    
+    
+    
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
+    
+    
+    
+    
+}
